aa0bee3869b7ccdf06ead42a6dfc41a0a199c3a3
Venkatshivasai <venkatshivasai10@gmail.com>
2023-12-01 08:39:37 +0000

    Text_Input

---
+++ src/components/TextInput/styledComponents.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components'
+
+export const AppContainer = styled.div`
+  background-color: #f5d0fe;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+export const CardContainer = styled.div`
+  background-color: #ffffff;
+  padding: 30px;
+  height: 200px;
+  border-radius: 10px;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+export const Heading = styled.h1`
+  font-family: 'Roboto';
+  font-size: 20px;
+`
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const Paragraph = styled.p`
+  font-size: 15px;
+  font-family: 'Roboto';
+  padding-right: 10px;
+`
+
+export const Input = styled.input`
+  height: 30px;
+  width: 250px;
+  outline: none;
+  margin-right: 20px;
+  padding: 10px;
+`
+
+export const Button = styled.button`
+  height: 30px;
+  width: 55px;
+  background-color: #d946ef;
+  color: #ffffff;
+  font-size: 13px;
+  font-family: 'Roboto';
+  cursor: pointer;
+`
